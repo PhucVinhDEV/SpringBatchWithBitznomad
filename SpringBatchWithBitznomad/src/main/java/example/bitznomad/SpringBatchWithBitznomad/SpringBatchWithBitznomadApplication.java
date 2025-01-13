17e92a9c2878cc4ac7e6d6176bc4524839bdb8da
oIoVinhGa <tpvinh292931@gmail.com>
2025-01-13 10:25:08 +0700

    init - project

---
+++ SpringBatchWithBitznomad/src/main/java/example/bitznomad/SpringBatchWithBitznomad/SpringBatchWithBitznomadApplication.java
@@ -0,0 +1,13 @@
+package example.bitznomad.SpringBatchWithBitznomad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchWithBitznomadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchWithBitznomadApplication.class, args);
+	}
+
+}
---
+++ SpringBatchWithBitznomad/src/test/java/example/bitznomad/SpringBatchWithBitznomad/SpringBatchWithBitznomadApplicationTests.java
@@ -0,0 +1,13 @@
+package example.bitznomad.SpringBatchWithBitznomad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchWithBitznomadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
